93678a0344afb078cc23d618266be38011d97080
FABIANA <Fabiana@FABIANAs-MacBook-Pro.local>
2016-10-07 11:42:07 +0700

    Pacman classgit add .

--- core/src/com/fabiana/game/GameScreen.java
+++ core/src/com/fabiana/game/GameScreen.java
@@ -5,26 +5,32 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter{
     private PacmanGame pacmanGame;
     private Texture pacmanImg;
-    private int x;
-    private int y;
+    private Pacman pacman;
+    
 
     public GameScreen(PacmanGame pacmangame){
         this.pacmanGame = pacmangame;
+        pacman = new Pacman(100,100);
         pacmanImg = new Texture("pacman.png");
-        x = 100;
-        y = 100;
     }
 
     public void update(float delta){
         if(Gdx.input.isKeyPressed(Keys.LEFT)){
-          x -= 10;
+          pacman.move(pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)){
-          x += 10;
+          pacman.move(pacman.DIRECTION_RIGHT);
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)){
+          pacman.move(pacman.DIRECTION_DOWN);
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)){
+          pacman.move(pacman.DIRECTION_UP);
         }
     }
 
@@ -35,7 +41,8 @@ public class GameScreen extends ScreenAdapter{
         update(delta);
         SpriteBatch batch = pacmanGame.batch;
         batch.begin();
-        batch.draw(pacmanImg, x, y);
+        Vector2 pos = pacman.getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
         batch.end();
 
     }
