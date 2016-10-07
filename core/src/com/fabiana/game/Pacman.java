d3af4e0ab532dda323540701cc64eca6a4eff598
FABIANA <Fabiana@FABIANAs-MacBook-Pro.local>
2016-10-07 11:51:29 +0700

    Pacman class v.2

--- core/src/com/fabiana/game/Pacman.java
+++ core/src/com/fabiana/game/Pacman.java
@@ -10,6 +10,7 @@ public class Pacman {
     public static final int DIRECTION_DOWN = 3;
     public static final int DIRECTION_LEFT = 4;
     public static final int DIRECTION_STILL = 0;
+    public static final int SPEED = 10;
 
     private static final int [][] DIR_DIFF = new int [][] {
         {0,0},
@@ -20,8 +21,8 @@ public class Pacman {
     };
 
     public void move(int dir){
-        position.x += 10 * DIR_DIFF[dir][0];
-        position.y += 10 * DIR_DIFF[dir][1];
+        position.x += SPEED * DIR_DIFF[dir][0];
+        position.y += SPEED * DIR_DIFF[dir][1];
     }
 
     public Pacman(int x,int y){
